1adbc47554011103c1fc2e42f5c98c06d07f8053
hiepnxph <hiepnxph@gmail.com>
2022-03-11 18:55:46 +0700

    uplate

---
+++ src/app.js
@@ -0,0 +1,19 @@
+// const express = require("express");
+import express from 'express';
+import productRouter from './routes/product';
+import mongoose from 'mongoose';
+const app = express();
+// middleware
+app.use(express.json());
+
+// Routing
+app.use("/api", productRouter);
+
+// connect database
+mongoose.connect("mongodb://localhost:27017/we16306")
+    .then(() => console.log("Connect db thanh cong"))
+// Connect
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Server${PORT} đang hoạt động`);
+});
